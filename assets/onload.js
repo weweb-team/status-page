492bb949b8da1bff5906a60f8e0c51b8907c4d29
adrien <adrien@weweb.io>
2025-06-17 18:29:25 +0200

    fix js

--- assets/onload.js
+++ assets/onload.js
@@ -1,7 +1,7 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function(event) {
     let allSystemElement = document.querySelector("main .up")
     if (allSystemElement !== null) {
         let cleanString = allSystemElement.innerHTML.replace("✅ &nbsp; ", "")
         let allSystemElement = allSystemElement.innerHTML = cleanString
     }
-}
+});
