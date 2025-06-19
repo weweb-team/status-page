1d258c04c752d7a216668c39ad174bf3fd646fce
Adrien GARCIA <adrien@weweb.io>
2025-06-19 09:52:07 +0200

    Update onload.js

--- onload.js
+++ onload.js
@@ -1,4 +1,14 @@
-if(document.querySelector("main .up") !== null) {
+function removeCheckMark() {
     let cleanString = document.querySelector("main .up").innerHTML.replace("✅ &nbsp; ", "")
     document.querySelector("main .up").innerHTML = cleanString
 }
+
+function pollForCheckMark() {
+    if(document.querySelector("main .up") !== null) {
+        removeCheckMark()
+    } else {
+        setTimeout(pollForCheckMark, 500)
+    }
+}
+
+pollForCheckMark()
